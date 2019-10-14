f5e9e9652424ad3606eb1216447319724f6d5bf1
neigegada <1184575830@qq.com>
2019-10-14 21:30:12 +0800

    写好了用户名重复验证功能，接下来需要把信息添加到数据库中

---
+++ taoshuwang/server/user.php
@@ -0,0 +1,18 @@
+<?php
+$user = $_REQUEST["username"];
+$db = mysqli_connect("127.0.0.1","root","","taoshuwang");
+$sql = "SELECT * FROM user WHERE username='$user' ";
+$result = mysqli_query($db, $sql);
+// print_r(mysqli_num_rows($result));
+$response = array("status"=>"","msg"=>"");
+if(mysqli_num_rows($result)==1){
+    /*用户名已被注册*/
+    $response["status"] = "error";
+    $response["msg"] = " 该用户名已经被注册！！";
+    echo json_encode($response,true);
+}else{
+    $response["status"] = "ok";
+    $response["msg"] = "可注册用户";
+    echo json_encode($response,true);
+}
+?>
